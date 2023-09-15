7baaf2d393781840e68cbede1d141b58634fc9f1
twinz09 <jhosembiring15@gmail.com>
2023-09-15 15:42:41 +0700

    membuat website avodrac collection

---
+++ script.js
@@ -0,0 +1,44 @@
+// Mengambil referensi ke semua tombol "Copy" dengan class "copy-button"
+const copyButtons = document.querySelectorAll(".copy-button");
+
+// Menambahkan event listener ke setiap tombol "Copy"
+copyButtons.forEach((button, index) => {
+  button.addEventListener("click", function () {
+    // Mengambil teks yang sesuai berdasarkan indeks tombol yang diklik
+    const textToCopy = document.getElementById(
+      `text-to-copy-${index + 1}`
+    ).innerText;
+
+    // Buat elemen textarea sementara
+    const tempTextarea = document.createElement("textarea");
+
+    // Set nilai teks di textarea sementara menjadi teks yang ingin Anda copy
+    tempTextarea.value = textToCopy;
+
+    // Menambahkan textarea sementara ke dokumen
+    document.body.appendChild(tempTextarea);
+
+    // Memilih teks di textarea
+    tempTextarea.select();
+
+    // Menyalin teks ke clipboard
+    document.execCommand("copy");
+
+    // Menghapus textarea sementara
+    document.body.removeChild(tempTextarea);
+
+    // Mengubah teks pada tombol menjadi "Copied"
+    button.innerText = "Copied";
+
+    // Menonaktifkan tombol setelah dicopy
+    button.disabled = true;
+
+    // Mengatur ulang teks tombol lainnya menjadi "Copy" dan mengaktifkannya
+    copyButtons.forEach((otherButton, otherIndex) => {
+      if (otherIndex !== index) {
+        otherButton.innerText = "Copy";
+        otherButton.disabled = false;
+      }
+    });
+  });
+});
